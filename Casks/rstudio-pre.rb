d5a725c22299aea2f8fc26b8c6b8fc59fa0a89bb
Albert Z Wang <albert.zhao.wang@gmail.com>
2016-08-22 12:53:05 -0400

    update rstudio-pre

--- Casks/rstudio-pre.rb
+++ Casks/rstudio-pre.rb
@@ -1,6 +1,6 @@
 cask 'rstudio-pre' do
-  version '0.99.1285'
-  sha256 '48cd158c885d7a4ffcfd94a742fba425130dc023cf4d30dde838b5f3cad2852d'
+  version '0.99.1290'
+  sha256 '5aa5d4bd686c5a365091447d943fd0aaebebec794d9f7c4565ef0f4958de79ce'
 
   # amazonaws.com/rstudio-dailybuilds was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-#{version}.dmg"
